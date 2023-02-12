07a22686de8fd049ea0f38c7e2bdfeee9424110a
shuto1998 <shuto.y0319@gmail.com>
2023-02-12 12:16:57 +0000

    finish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,9 +7,10 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-    redirect_to list_path(list.id)
+    redirect_to list_path(@list.id)
     else
     render :new
+
     end
   end
 
